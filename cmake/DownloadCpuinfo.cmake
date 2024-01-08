94f710b9d51912061b25df16ab56aa3d983bf805
Phoebe Chen <phoebe.chen@sifive.com>
2024-01-08 03:29:08 -0800

    Update cpuinfo to 313524ab20d2041854af8ad07bf726ddd485d258.
    Signed-off-by: Phoebe Chen <phoebe.chen@sifive.com>

--- cmake/DownloadCpuinfo.cmake
+++ cmake/DownloadCpuinfo.cmake
@@ -17,8 +17,8 @@ ENDIF()
 
 INCLUDE(ExternalProject)
 ExternalProject_Add(cpuinfo
-  URL https://github.com/pytorch/cpuinfo/archive/d6860c477c99f1fce9e28eb206891af3c0e1a1d7.zip
-  URL_HASH SHA256=a615cac78fad03952cc3e1fd231ce789a8df6e81a5957b64350cb8200364b385
+  URL https://github.com/pytorch/cpuinfo/archive/313524ab20d2041854af8ad07bf726ddd485d258.zip
+  URL_HASH SHA256=4145872f683d649a8f8a1c4fbe7cf179d9b77aa80690da962a80682098c82189
   SOURCE_DIR "${CMAKE_BINARY_DIR}/cpuinfo-source"
   BINARY_DIR "${CMAKE_BINARY_DIR}/cpuinfo"
   CONFIGURE_COMMAND ""
